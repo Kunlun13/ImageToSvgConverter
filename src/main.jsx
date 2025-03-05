c56c1e693c6cf341e3e83e0ed81328142aeb74c1
Kunlun13 <kunaldavinci13092002@gmail.com>
2025-03-06 00:36:37 +0530

    Add the favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import { RouterProvider } from 'react-router'
 import Layout from './components/Layout/Layout.jsx'
